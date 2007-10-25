ff5e2a91d0274b450439488854104ecc72ba6db4
jaroslavl1 <jaroslavl1@44667b30-4a37-0410-a875-8fd7e1d9ee37>
2007-10-25 19:34:27 +0000

    More wrapper functions finished for portaudio driver.

---
+++ branches/audio-refactoring/sipXmediaLib/src/mp/MpHostAudioApiInfo.cpp
@@ -0,0 +1,46 @@
+//
+// Copyright (C) 2007 Jaroslav Libak
+//
+// $$
+///////////////////////////////////////////////////////////////////////////////
+
+// SYSTEM INCLUDES
+// APPLICATION INCLUDES
+#include "mp/MpHostAudioApiInfo.h"
+
+// DEFINES
+// EXTERNAL FUNCTIONS
+// EXTERNAL VARIABLES
+// CONSTANTS
+// STATIC VARIABLE INITIALIZATIONS
+// MACROS
+// GLOBAL VARIABLES
+// GLOBAL FUNCTIONS
+
+/* //////////////////////////// PUBLIC //////////////////////////////////// */
+
+/* ============================ CREATORS ================================== */
+
+MpHostAudioApiInfo::MpHostAudioApiInfo()
+{
+
+}
+
+MpHostAudioApiInfo::~MpHostAudioApiInfo(void)
+{
+
+}
+
+/* ============================ MANIPULATORS ============================== */
+
+/* ============================ ACCESSORS ================================= */
+
+/* ============================ INQUIRY =================================== */
+
+/* //////////////////////////// PROTECTED ///////////////////////////////// */
+
+/* //////////////////////////// PRIVATE /////////////////////////////////// */
+
+/* ============================ FUNCTIONS ================================= */
+
+
