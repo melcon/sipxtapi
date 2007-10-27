54fee39660cef12f0f06a494186d15675de5c21c
jaroslavl1 <jaroslavl1@44667b30-4a37-0410-a875-8fd7e1d9ee37>
2007-10-27 11:07:43 +0000

    Added basic portlib callback holder MpPortAudioStream. It will be used only to hold buffers & callback, for nothing else. It wont have any stream operations.
    Deleted other stream files, as portaudio is not really suitable for separating output/input streams into different classes. This driver is meant for portaudio with possibility to use different one if one can implement required methods in MpAudioDriverBase.

---
+++ branches/audio-refactoring/sipXmediaLib/include/mp/MpPortAudioStream.h
@@ -0,0 +1,90 @@
+//
+// Copyright (C) 2007 Jaroslav Libak
+//
+// $$
+///////////////////////////////////////////////////////////////////////////////
+
+#ifndef MpPortAudioStream_h__
+#define MpPortAudioStream_h__
+
+// SYSTEM INCLUDES
+// APPLICATION INCLUDES
+#include <utl/UtlBool.h>
+#include "portaudio.h"
+
+// DEFINES
+// EXTERNAL FUNCTIONS
+// EXTERNAL VARIABLES
+// CONSTANTS
+// FORWARD DECLARATIONS
+// STRUCTS
+// TYPEDEFS
+// MACROS
+// GLOBAL VARIABLES
+// GLOBAL FUNCTIONS
+
+/**
+* Representation of asynchronous portlib audio stream. For synchronous streams, it is not
+* needed. Stream can be output/input/full duplex. It is only used for callback.
+*/
+class MpPortAudioStream
+{
+   /* //////////////////////////// PUBLIC //////////////////////////////////// */
+public:
+   /* ============================ CREATORS ================================== */
+   ///@name Creators
+   //@{
+
+   /// Constructor.
+   MpPortAudioStream(UtlBoolean isOutput, UtlBoolean isInput);
+
+   /// Destructor.
+   virtual ~MpPortAudioStream(void);
+   //@}
+
+   /* ============================ MANIPULATORS ============================== */
+   ///@name Manipulators
+   //@{
+
+   /**
+    * audio callback according to portaudio spec. In user data it receives
+    * pointer to MpPortAudioStream instance
+    */
+   static int streamCallback(const void *input,
+                             void *output,
+                             unsigned long frameCount,
+                             const PaStreamCallbackTimeInfo* timeInfo,
+                             PaStreamCallbackFlags statusFlags,
+                             void *userData );
+
+   //@}
+
+   /* ============================ ACCESSORS ================================= */
+   ///@name Accessors
+   //@{
+   //@}
+   /* ============================ INQUIRY =================================== */
+   ///@name Inquiry
+   //@{
+   //@}
+
+   /* //////////////////////////// PROTECTED ///////////////////////////////// */
+protected:
+
+   /* //////////////////////////// PRIVATE /////////////////////////////////// */
+private:
+
+   /**
+    * Stream callback of this instance to support multiple streams.
+    */
+   int instanceStreamCallback(const void *input,
+                              void *output,
+                              unsigned long frameCount,
+                              const PaStreamCallbackTimeInfo* timeInfo,
+                              PaStreamCallbackFlags statusFlags);
+
+   UtlBoolean m_isOutput; ///< whether this stream is output
+   UtlBoolean m_isInput; ///< whether this stream is input
+};
+
+#endif // MpPortAudioStream_h__
