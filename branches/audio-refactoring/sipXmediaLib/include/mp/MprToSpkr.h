1467656132e32c5e500d5c21b381601281b3f2cd
jaroslavl1 <jaroslavl1@44667b30-4a37-0410-a875-8fd7e1d9ee37>
2007-11-03 21:35:51 +0000

    MprFromMic has been migrated to portaudio driver. MpMisc has beed updated to initialize new MpAudioDriverManager.
    MpAudioDriverManager is used to create audio driver, delete it, create initial audio streams, close streams at the end. It also keeps input/output audio stream Ids.
    
    MprToSpkr was partially cleaned up, its not finished yet.
    
    Still not functional or compilable.

--- branches/audio-refactoring/sipXmediaLib/include/mp/MprToSpkr.h
+++ branches/audio-refactoring/sipXmediaLib/include/mp/MprToSpkr.h
@@ -15,17 +15,11 @@
 #ifndef _MprToSpkr_h_
 #define _MprToSpkr_h_
 
-#define DETECT_SPKR_OVERFLOW
-#undef DETECT_SPKR_OVERFLOW
-// #define DETECT_SPKR_OVERFLOW  //MG 10-3-01, trying to understand where the signal clips
-
 // SYSTEM INCLUDES
-
 // APPLICATION INCLUDES
 #include "os/OsStatus.h"
 #include "os/OsNotification.h"
 #include "os/OsMsgQ.h"
-//#include "mp/MpMisc.h"
 #include "mp/MpAudioResource.h"
 #include "mp/MpFlowGraphMsg.h"
 #include "mp/MpCodec.h"
@@ -37,7 +31,6 @@
 // CONSTANTS
 // STRUCTS
 // TYPEDEFS
-typedef void (*TOSPEAKERHOOK)(const int nLength, short* samples) ;
 // FORWARD DECLARATIONS
 
 /**
@@ -48,9 +41,6 @@ class MprToSpkr : public MpAudioResource
 {
 /* //////////////////////////// PUBLIC //////////////////////////////////// */
 public:
-   enum{VOLUME_CONTROL_TABLE_SIZE=64};
-   enum{MIN_SPKR_DtoA=-32767};
-   enum{MAX_SPKR_DtoA= 32767};
 
 /* ============================ CREATORS ================================== */
 ///@name Creators
@@ -68,19 +58,6 @@ public:
    // acknowledgements in bursts covering 60 to 100 msecs at once.  At
    // 10 msec per buffer, this resulted in frequent starvation.
    enum { MAX_SPKR_BUFFERS = 12 };
-   enum { MIN_SPKR_BUFFERS = 1 };
-   enum { SKIP_SPKR_BUFFERS = 1 };
-
-#ifdef REIMPLEMENT_CLARISIS_EQ /* [ */
-   enum {EqFilterLen_ix = 24};
-
-   static       int  smClarisisHandsetSpeakerEq[EqFilterLen_ix];
-   int*              mpEqSave_ix;
-   int               mLastSpkr_ix;
-   int*              mpCurEq_ix;
-
-   void SpeakerEqualization_ix(MpAudioSample* samples, int iLength);
-#endif /* REIMPLEMENT_CLARISIS_EQ ] */
 
    typedef enum {
       ATTEN_LOUDEST = 0,    ///< 0 dB, no attenuation
@@ -94,10 +71,6 @@ public:
 //@{
 
 //@}
-
-#ifdef DETECT_SPKR_OVERFLOW /* [ */
-    static int spkrStats();
-#endif /* DETECT_SPKR_OVERFLOW ] */
 
 /* ============================ ACCESSORS ================================= */
 ///@name Accessors
@@ -116,45 +89,11 @@ protected:
 
 /* //////////////////////////// PRIVATE /////////////////////////////////// */
 private:
-   typedef enum
-   {
-      PLAY_FILE = MpFlowGraphMsg::RESOURCE_SPECIFIC_START
-   } AddlMsgTypes;
+   OsMsgQ* mpEchoQ;       ///< Audio data will be sent to this queue too.
+                          ///< This queue should be connected to Echo
+                          ///< Cancelation resource.
 
-   enum{EqFilterLen = 24};
-
-
-   OsMsgQ*         mpSpkQ;        ///< Audio data will be sent to this queue.
-   OsMsgQ*         mpEchoQ;       ///< Audio data will be sent to this queue too.
-                                  ///<  This queue should be connected to Echo
-                                  ///<  Cancelation resource.
-
-   static int      slInitVol;
-   static int      slVolStep;
-   uint32_t        mulNoiseLevel; ///< Used in comfort noise generation.
-
-   enum{MAX_SUPPRESSION = 7};
-   int             mlpVolTable[VOLUME_CONTROL_TABLE_SIZE];
-                                     ///< volume levels + a mute level
-
-#ifdef DETECT_SPKR_OVERFLOW // [
-
-   static int smStatsReports;
-
-   int        mOverflowsIn;
-   int        mUnderflowsIn;
-   int        mOverflowsOut;
-   int        mUnderflowsOut;
-   int        mTotalSamples;
-   int        mMaxIn;
-   int        mMinIn;
-   int        mMaxOut;
-   int        mMinOut;
-   int        mReport;
-
-   void stats(void);
-
-#endif // DETECT_SPKR_OVERFLOW ]
+   uint32_t mulNoiseLevel; ///< Used in comfort noise generation.
 
    virtual UtlBoolean doProcessFrame(MpBufPtr inBufs[],
                                      MpBufPtr outBufs[],
@@ -164,16 +103,11 @@ private:
                                      int samplesPerFrame=80,
                                      int samplesPerSecond=8000);
 
-   void initVolTable(void);
-
      /// Copy constructor (not implemented for this class)
    MprToSpkr(const MprToSpkr& rMprToSpkr);
 
      /// Assignment operator (not implemented for this class)
    MprToSpkr& operator=(const MprToSpkr& rhs);
-
-public:
-    static TOSPEAKERHOOK s_fnToSpeakerHook ;
 
 };
 
