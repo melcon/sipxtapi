661465daf533fa0460ca1edbf13fbf96eb022c1f
jaroslavl1 <jaroslavl1@44667b30-4a37-0410-a875-8fd7e1d9ee37>
2008-10-18 17:19:36 +0000

    Fixed unit tests related to lines, after SipLine class changed. Also we cannot invoke SipLineMgr methods by passing just string, as then string is parsed incorrectly (with <>, as by default we parse url and not uri. We either have to construct Url as uri, or remove brackets ourselves), and line lookup fails.

--- sipXtackLib/src/test/net/SipLineListTest.cpp
+++ sipXtackLib/src/test/net/SipLineListTest.cpp
@@ -21,12 +21,12 @@
 #include <cppunit/TestCase.h>
 
 // DEFINES
-#define USER_ENTERED_URI_1 "\"John Doe\"<sip:user1:password1@host1:5060;urlparm=value1?headerParam=value1>;fieldParam=value1"
-#define USER_ENTERED_URI_2 "\"Jane Doe\"<sip:user2:password2@host2:5061;urlparm=value2?headerParam=value2>;fieldParam=value2"
+#define FULL_LINE_URL_1 "\"John Doe\"<sip:user1:password1@host1:5060;urlparm=value1?headerParam=value1>;fieldParam=value1"
+#define FULL_LINE_URL_2 "\"Jane Doe\"<sip:user2:password2@host2:5061;urlparm=value2?headerParam=value2>;fieldParam=value2"
 #define USER_ID_1 "user1"
 #define USER_ID_2 "user2"
-#define IDENTITY_URI_1 "<sip:user1:password1@host1:5060;urlparm=value1"
-#define IDENTITY_URI_2 "<sip:user2:password2@host2:5061;urlparm=value2>"
+#define IDENTITY_URI_1 "sip:user1@host1"
+#define IDENTITY_URI_2 "sip:user2@host2"
 
 // EXTERNAL FUNCTIONS
 // EXTERNAL VARIABLES
@@ -70,8 +70,8 @@ public:
 
    void testAddRemove()
    {
-      SipLine line1(USER_ENTERED_URI_1, IDENTITY_URI_1);
-      SipLine line2(USER_ENTERED_URI_2, IDENTITY_URI_2);
+      SipLine line1(FULL_LINE_URL_1);
+      SipLine line2(FULL_LINE_URL_2);
       SipLineList lineList;
 
       CPPUNIT_ASSERT(lineList.add(line1));
@@ -79,13 +79,13 @@ public:
       // try remove
       CPPUNIT_ASSERT(lineList.remove(line1));
       CPPUNIT_ASSERT(!lineList.remove(line1)); // 2nd must fail
-      CPPUNIT_ASSERT(lineList.remove(line2.getIdentityUri())); // remove by uri
+      CPPUNIT_ASSERT(lineList.remove(line2.getLineUri())); // remove by uri
    }
 
    void testAddTwice()
    {
-      SipLine line1(USER_ENTERED_URI_1, IDENTITY_URI_1);
-      SipLine line2(USER_ENTERED_URI_2, IDENTITY_URI_2);
+      SipLine line1(FULL_LINE_URL_1);
+      SipLine line2(FULL_LINE_URL_2);
       SipLineList lineList;
 
       CPPUNIT_ASSERT(lineList.add(line1));
@@ -101,22 +101,23 @@ public:
 
    void testFindLine()
    {
-      SipLine line1(USER_ENTERED_URI_1, IDENTITY_URI_1);
+      SipLine line1(FULL_LINE_URL_1);
+      Url lineUri = line1.getLineUri();
       SipLineList lineList;
       lineList.add(line1);
       
       // try find by lineID
       CPPUNIT_ASSERT(lineList.findLine(line1.getLineId(), "xxx", "xxx"));
       // find by uri
-      CPPUNIT_ASSERT(lineList.findLine("xxx", IDENTITY_URI_1, "xxx"));
+      CPPUNIT_ASSERT(lineList.findLine("xxx", lineUri, "xxx"));
       // find by userID
       CPPUNIT_ASSERT(lineList.findLine("xxx", "xxx", USER_ID_1));
    }
 
    void testGetLineCount()
    {
-      SipLine line1(USER_ENTERED_URI_1, IDENTITY_URI_1);
-      SipLine line2(USER_ENTERED_URI_2, IDENTITY_URI_2);
+      SipLine line1(FULL_LINE_URL_1);
+      SipLine line2(FULL_LINE_URL_2);
       SipLineList lineList;
 
       CPPUNIT_ASSERT(lineList.getLinesCount() == 0);
@@ -130,17 +131,17 @@ public:
 
    void testGetLine()
    {
-      SipLine line1(USER_ENTERED_URI_1, IDENTITY_URI_1);
+      SipLine line1(FULL_LINE_URL_1);
       SipLineList lineList;
 
       lineList.add(line1);
       CPPUNIT_ASSERT(lineList.getLine(line1.getLineId()));
-      CPPUNIT_ASSERT(lineList.getLine(line1.getIdentityUri()));
+      CPPUNIT_ASSERT(lineList.getLine(line1.getLineUri()));
    }
 
    void testGetLineByUserId()
    {
-      SipLine line1(USER_ENTERED_URI_1, IDENTITY_URI_1);
+      SipLine line1(FULL_LINE_URL_1);
       SipLineList lineList;
 
       lineList.add(line1);
@@ -150,11 +151,11 @@ public:
 
    void testLineExists()
    {
-      SipLine line1(USER_ENTERED_URI_1, IDENTITY_URI_1);
+      SipLine line1(FULL_LINE_URL_1);
       SipLineList lineList;
 
       lineList.add(line1);
-      CPPUNIT_ASSERT(lineList.lineExists(line1.getIdentityUri()));
+      CPPUNIT_ASSERT(lineList.lineExists(line1.getLineUri()));
       CPPUNIT_ASSERT(!lineList.lineExists("xxx"));
    }
 };
