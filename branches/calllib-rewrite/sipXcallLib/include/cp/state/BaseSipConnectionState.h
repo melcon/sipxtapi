6a7dde8a4d6d561fbaa120b90b8fb24135bd3c0e
jaroslavl1 <jaroslavl1@44667b30-4a37-0410-a875-8fd7e1d9ee37>
2008-12-03 23:29:52 +0000

    Added skeleton handlers for concrete sip requests and 1xx,2xx,3xx responses to concrete responses. 4xx,5xx,6xx will be handled automatically. We might have to allow 4xx,5xx,6xx processing in concrete response handlers as well for event firing.

--- branches/calllib-rewrite/sipXcallLib/include/cp/state/BaseSipConnectionState.h
+++ branches/calllib-rewrite/sipXcallLib/include/cp/state/BaseSipConnectionState.h
@@ -134,6 +134,42 @@ protected:
    /** Sends specified sip message, updating the state of SipDialog. SipMessage will be updated. */
    UtlBoolean sendMessage(SipMessage& sipMessage);
 
+   /** Handles SIP request and response messages. */
+   virtual SipConnectionStateTransition* processSipMessage(const SipMessage& sipMessage);
+
+   /**
+    * Processes inbound SIP request message. This method calls special handlers for each supported
+    * method that should be overridden in concrete state.
+    */
+   virtual SipConnectionStateTransition* processRequest(const SipMessage& sipMessage);
+
+   /** Handles inbound SIP INVITE requests */
+   virtual SipConnectionStateTransition* processInviteRequest(const SipMessage& sipMessage);
+
+   /** Handles inbound SIP UPDATE requests */
+   virtual SipConnectionStateTransition* processUpdateRequest(const SipMessage& sipMessage);
+
+   /** Handles inbound SIP ACK requests */
+   virtual SipConnectionStateTransition* processAckRequest(const SipMessage& sipMessage);
+
+   /** Handles inbound SIP BYE requests */
+   virtual SipConnectionStateTransition* processByeRequest(const SipMessage& sipMessage);
+
+   /** Handles inbound SIP CANCEL requests */
+   virtual SipConnectionStateTransition* processCancelRequest(const SipMessage& sipMessage);
+
+   /** Handles inbound SIP INFO requests */
+   virtual SipConnectionStateTransition* processInfoRequest(const SipMessage& sipMessage);
+
+   /** Handles inbound SIP NOTIFY requests */
+   virtual SipConnectionStateTransition* processNotifyRequest(const SipMessage& sipMessage);
+
+   /** Handles inbound SIP REFER requests */
+   virtual SipConnectionStateTransition* processReferRequest(const SipMessage& sipMessage);
+
+   /** Handles inbound SIP PRACK requests */
+   virtual SipConnectionStateTransition* processPrackRequest(const SipMessage& sipMessage);
+
    /**
     * Processes inbound SIP response message. This is the default handler than needs to
     * be called first, if this method is overridden. If this method handles the response,
@@ -142,8 +178,45 @@ protected:
     *
     * Implementation of this method is based on RFC5057. This method is responsible for
     * transition to disconnected state if some error response is received.
+    *
+    * This method handles error responses in a method independent way, terminating transaction
+    * or dialog if needed. Response codes 1xx, 2xx and 3xx are expected to be handled by
+    * method specific handlers. ACK doesn't have response.
     */
    virtual SipConnectionStateTransition* processResponse(const SipMessage& sipMessage);
+
+   /** Handles non error inbound SIP responses */
+   virtual SipConnectionStateTransition* processNonErrorResponse(const SipMessage& sipMessage);
+
+   /** Handles inbound SIP INVITE responses */
+   virtual SipConnectionStateTransition* processInviteResponse(const SipMessage& sipMessage);
+
+   /** Handles inbound SIP UPDATE responses */
+   virtual SipConnectionStateTransition* processUpdateResponse(const SipMessage& sipMessage);
+
+   /** Handles inbound SIP BYE responses */
+   virtual SipConnectionStateTransition* processByeResponse(const SipMessage& sipMessage);
+
+   /** Handles inbound SIP CANCEL responses */
+   virtual SipConnectionStateTransition* processCancelResponse(const SipMessage& sipMessage);
+
+   /** Handles inbound SIP INFO responses */
+   virtual SipConnectionStateTransition* processInfoResponse(const SipMessage& sipMessage);
+
+   /** Handles inbound SIP OPTIONS responses */
+   virtual SipConnectionStateTransition* processOptionsResponse(const SipMessage& sipMessage);
+
+   /** Handles inbound SIP NOTIFY responses */
+   virtual SipConnectionStateTransition* processNotifyResponse(const SipMessage& sipMessage);
+
+   /** Handles inbound SIP REFER responses */
+   virtual SipConnectionStateTransition* processReferResponse(const SipMessage& sipMessage);
+
+   /** Handles inbound SIP PRACK responses */
+   virtual SipConnectionStateTransition* processPrackResponse(const SipMessage& sipMessage);
+
+   /** Called when authentication retry occurs. We need to update dialogs. */
+   virtual SipConnectionStateTransition* handleAuthenticationRetryEvent(const SipMessage& sipMessage);
 
    /** Returns TRUE if some SIP method is allowed (may be sent) */
    UtlBoolean isMethodAllowed(const UtlString& sMethod);
@@ -174,6 +247,9 @@ protected:
 
    /** Quick access to sip call-id */
    UtlString getCallId() const;
+
+   /** Checks if given sip method needs transaction tracking */
+   UtlBoolean needsTransactionTracking(const UtlString& sipMethod) const;
 
    SipConnectionStateContext& m_rStateContext; ///< context containing state of sip connection. Needs to be locked when accessed.
    SipUserAgent& m_rSipUserAgent; // for sending sip messages
