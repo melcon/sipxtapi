8e3404b28dca6497c533257884b751489d665779
jaroslavl1 <jaroslavl1@44667b30-4a37-0410-a875-8fd7e1d9ee37>
2009-01-10 15:08:17 +0000

    Merged DTMF detector from SIP foundry from Keith Kyzivat (based on new wikipedia algorithm) with old DTMF detector. This seems to provide best of the both worlds. Subjectively this DTMF detector performs better than the old DTMF detector (less false detections, and more correct detections).
    Tested with wxCommunicator (with sipxtapi calllib rewrite), SJPhone 1.65 and X-Lite. All inband DTMF tones were detected over G.711a codec from all softphones, with no false tones being detected. Old DTMF detector was unable to detect digit 4 and 5 from SJPhone, but new DTMF detector works correctly.
    It also seems to have higher resistance to echo.
    
    Modified license to add all involved parties.

--- branches/calllib-rewrite/sipXmediaLib/include/mp/MprSimpleDtmfDetector.h
+++ branches/calllib-rewrite/sipXmediaLib/include/mp/MprSimpleDtmfDetector.h
@@ -1,8 +1,15 @@
-//
+//  
+// Copyright (C) 2007 SIPfoundry Inc. 
+// Licensed by SIPfoundry under the LGPL license. 
+//  
+// Copyright (C) 2007 SIPez LLC. 
+// Licensed to SIPfoundry under a Contributor Agreement. 
+// 
 // Copyright (C) 2007 stipus@stipus.com
-//
-// $$
-///////////////////////////////////////////////////////////////////////////////
+// $$ 
+////////////////////////////////////////////////////////////////////////////// 
+
+// Author: Keith Kyzivat <kkyzivat AT SIPez DOT com>
 
 #ifndef _MprDecodeInBandDtmf_h_
 #define _MprDecodeInBandDtmf_h_
@@ -37,8 +44,8 @@ public:
 
      //:Constructor
    MprSimpleDtmfDetector(const UtlString& rName, 
-                       int samplesPerFrame,
-                       int samplesPerSec);
+                         int samplesPerFrame,
+                         int samplesPerSec);
 
      //:Destructor
    virtual ~MprSimpleDtmfDetector();
@@ -48,13 +55,17 @@ public:
 /* ============================ MANIPULATORS ============================== */
 ///@name Manipulators
 //@{
+   /// Reset the state of the detector.
+   void reset();
+   /**<
+   *  Reset all accumulators, last detected DTMF, and recalculate coefficients.
+   */
 
 //@}
 
 /* ============================ ACCESSORS ================================= */
 ///@name Accessors
 //@{
-
 //@}
 
 /* ============================ INQUIRY =================================== */
@@ -77,20 +88,6 @@ protected:
 /* //////////////////////////// PRIVATE /////////////////////////////////// */
 private:
 
-   int m_dtmfLastDigit;
-   int m_sameDtmfDigitCount;
-   double Mk697;
-   double Mk770;
-   double Mk852;
-   double Mk941;
-   double Mk1209;
-   double Mk1336;
-   double Mk1477;
-   double Mk1633;
-
-   /// Goertzel
-   double Goertzel(const MpAudioSample *input, int numsamples, double mk);
-
    /// Handle messages for this resource.
    virtual UtlBoolean handleMessage(MpFlowGraphMsg& rMsg);
 
@@ -100,6 +97,58 @@ private:
      /// Assignment operator (not implemented for this class)
    MprSimpleDtmfDetector& operator=(const MprSimpleDtmfDetector& rhs);
 
+   /// Calculate coefficients needed for the goertzel algorithm
+   void calcCoeffs();
+   /**<
+   *  These coefficients are dependent on the sample rate, so new coefficients
+   *  need to be calculated whenever the sample rate changes.
+   *
+   *  From Wikipedia:
+   *
+   * coef = 2.0 * cos( (2.0 * PI * k) / (float)GOERTZEL_N)) ;
+   * Where k = (int) (0.5 + ((float)GOERTZEL_N * target_freq) / SAMPLING_RATE));
+   *
+   * More simply: coef = 2.0 * cos( (2.0 * PI * target_freq) / SAMPLING_RATE );
+   */
+
+   /// Validate the detected frequencies detected by processSample.
+   void dtmfValidation();
+   /**<
+   *  This looks at the detected frequencies, and determines if it conforms
+   *  to DTMF rules as specified by bell, and others -- i.e. can work well
+   *  with PSTN.
+   */
+
+   /// Process a sample through the detector.
+   UtlBoolean processSample(const MpAudioSample sample);
+   /**<
+   *  When getNumProcessSamples() samples are processed, a DTMF tone is either
+   *  detected or not.  In either case, the stored last DTMF tone detected is
+   *  overwritten with the current detection value.
+   *  
+   *  @return /p FALSE if this has not yet processed a multiple of getNumProcessSamples(), 
+   *          /p TRUE if this has processed a multiple of getNumProcessSamples()
+   *  
+   */
+
+   /** Returns TRUE if DTMF notification should be sent. */
+   UtlBoolean shouldSendNotification();
+
+   unsigned m_numProcessSamples;
+   uint32_t m_sampleCount;
+
+   static double ms_freqs_to_detect[]; 
+   static uint8_t ms_nFreqsToDetect;
+
+   double* m_q1;
+   double* m_q2;
+   double* m_r;
+
+   double* m_coefs;
+
+   char m_currentDtmfDigit; ///< remembers current detected DTMF digit
+   char m_lastDtmfDigit; ///< remembers last detected DTMF digit
+   int m_sameDtmfDigitCount;
 };
 
 /* ============================ INLINE METHODS ============================ */
