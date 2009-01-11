7dee6687674b132d0fdf6b65fbd86dfb7dd914c4
jaroslavl1 <jaroslavl1@44667b30-4a37-0410-a875-8fd7e1d9ee37>
2009-01-11 17:11:06 +0000

    Merged revisions 1134,1137,1139-1140 via svnmerge from https://sipxtapi.googlecode.com/svn/branches/calllib-rewrite
    ........
      r1134 | jaroslavl1 | 2009-01-11 02:04:23 +0100 (So, 11 Jän 2009) | 1 line
      
      Allow CANCEL retransmissions even after provisional response is received.
    ........
      r1137 | jaroslavl1 | 2009-01-11 16:21:53 +0100 (So, 11 Jän 2009) | 3 lines
      
      Fixed bug with handling of duplicate requests in sipxtacklib. For all requests except for ACK, resend the last final response if available, or the last provisional response.
      
      Without this sipxtacklib would swallow request retransmits - it would not respond, and would not pass them to observers.
    ........
      r1139 | jaroslavl1 | 2009-01-11 17:55:54 +0100 (So, 11 Jän 2009) | 1 line
      
      Send REGISTER with cseq num starting from 1, not 101.
    ........
      r1140 | jaroslavl1 | 2009-01-11 18:04:35 +0100 (So, 11 Jän 2009) | 1 line
      
      Only set Allow and Supported fields for INVITE, SUBSCRIBE, OPTIONS requests, and provisional or final 2x responses.
    ........

--- sipXtackLib/src/net/SipUserAgent.cpp
+++ sipXtackLib/src/net/SipUserAgent.cpp
@@ -1518,7 +1518,10 @@ void SipUserAgent::dispatch(SipMessage* message, int messageType, SIPX_TRANSPORT
       delete message;
       return;
    }
-
+#ifdef PRINT_SIP_MESSAGE
+   enableConsoleOutput(TRUE);
+   osPrintf("\nSipUserAgent::dispatch\n%s\n-----------------------------------\n", message->toString().data());
+#endif
    int len;
    UtlString msgBytes;
    UtlString messageStatusString;
@@ -1662,23 +1665,20 @@ void SipUserAgent::dispatch(SipMessage* message, int messageType, SIPX_TRANSPORT
       eventTimes.addEvent("handling TX");
 #endif
       // This is a message that was already recieved once
-      if (   transaction
-         && relationship == SipTransaction::MESSAGE_DUPLICATE
-         )
+      if (transaction && relationship == SipTransaction::MESSAGE_DUPLICATE)
       {
+         UtlString seqMethod;
+         int seqNum;
+         message->getCSeqField(&seqNum, &seqMethod);
          // Resends of final INVITE responses need to be
          // passed through if they are 2xx class or the ACk
          // needs to be resent if it was a failure (i.e. 3xx,4xx,5xx,6xx)
-         if(message->isResponse())
+         if (message->isResponse())
          {
             int responseCode = message->getResponseStatusCode();
-            UtlString transactionMethod;
-            int respCseq;
-            message->getCSeqField(&respCseq, &transactionMethod);
 
-            if (   responseCode >= SIP_2XX_CLASS_CODE 
-               && transactionMethod.compareTo(SIP_INVITE_METHOD) == 0
-               )
+            if (responseCode >= SIP_2XX_CLASS_CODE &&
+               seqMethod.compareTo(SIP_INVITE_METHOD) == 0)
             {
                transaction->handleIncoming(*message,
                   *this,
@@ -1688,6 +1688,25 @@ void SipUserAgent::dispatch(SipMessage* message, int messageType, SIPX_TRANSPORT
                   pTransport);
 
                // Should never dispatch a resendof a 2xx
+               if(delayedDispatchMessage)
+               {
+                  delete delayedDispatchMessage;
+                  delayedDispatchMessage = NULL;
+               }
+            }
+         }
+         else
+         {
+            // is a request, but not ACK
+            if (seqMethod.compareTo(SIP_ACK_METHOD) != 0)
+            {
+               // resend last final response
+               transaction->handleIncoming(*message,
+                  *this,
+                  relationship,
+                  mSipTransactions,
+                  delayedDispatchMessage,
+                  pTransport);
                if(delayedDispatchMessage)
                {
                   delete delayedDispatchMessage;
@@ -4675,11 +4694,13 @@ void SipUserAgent::addAgentCapabilities(SipMessage& sipMessage) const
    UtlString allowedMethodsSet;
    if (!sipMessage.getAllowField(allowedMethodsSet) && mbAllowHeader)
    {
-      if(seqMethod.compareTo(SIP_CANCEL_METHOD) != 0 &&
-         seqMethod.compareTo(SIP_ACK_METHOD) != 0)
+      // only set Allow: for INVITE, SUBSCRIBE, OPTIONS
+      if(seqMethod.compareTo(SIP_INVITE_METHOD) == 0 ||
+         seqMethod.compareTo(SIP_SUBSCRIBE_METHOD) == 0 ||
+         seqMethod.compareTo(SIP_OPTIONS_METHOD) == 0)
       {
          if (!bIsResponse ||
-            (bIsResponse && responseStatusCode > SIP_1XX_CLASS_CODE))
+            (bIsResponse && responseStatusCode > SIP_1XX_CLASS_CODE && responseStatusCode < SIP_3XX_CLASS_CODE))
          {
             UtlString allowedMethods;
             getAllowedMethods(&allowedMethods);
@@ -4688,20 +4709,22 @@ void SipUserAgent::addAgentCapabilities(SipMessage& sipMessage) const
       }
    }
 
-   // Set the supported extensions if this is not
-   // an ACK or NOTIFY request and the Supported field 
-   // is not already set.
-   if(seqMethod.compareTo(SIP_ACK_METHOD) &&
-      !sipMessage.getHeaderValue(0, SIP_SUPPORTED_FIELD))
+   if(!sipMessage.getHeaderValue(0, SIP_SUPPORTED_FIELD))
    {
-      if (!bIsResponse ||
-         (bIsResponse && responseStatusCode > SIP_1XX_CLASS_CODE && responseStatusCode < SIP_3XX_CLASS_CODE))
+      // only set Supported: for INVITE, SUBSCRIBE, OPTIONS
+      if(seqMethod.compareTo(SIP_INVITE_METHOD) == 0 ||
+         seqMethod.compareTo(SIP_SUBSCRIBE_METHOD) == 0 ||
+         seqMethod.compareTo(SIP_OPTIONS_METHOD) == 0)
       {
-         UtlString supportedExtensions;
-         getSupportedExtensions(supportedExtensions);
-         if (supportedExtensions.length() > 0)
+         if (!bIsResponse ||
+            (bIsResponse && responseStatusCode > SIP_1XX_CLASS_CODE && responseStatusCode < SIP_3XX_CLASS_CODE))
          {
-            sipMessage.setSupportedField(supportedExtensions);
+            UtlString supportedExtensions;
+            getSupportedExtensions(supportedExtensions);
+            if (supportedExtensions.length() > 0)
+            {
+               sipMessage.setSupportedField(supportedExtensions);
+            }
          }
       }
    }
