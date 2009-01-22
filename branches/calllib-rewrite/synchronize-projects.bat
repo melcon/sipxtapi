5bb77f31d13dddce7ad572a59ff8a3bb6d80ef14
jaroslavl1 <jaroslavl1@44667b30-4a37-0410-a875-8fd7e1d9ee37>
2009-01-22 21:55:50 +0000

    Removed VS2003 solution and project files, it looks like nobody needs them anymore, and its difficult to keep the solution up to date without VS2003. If anybody requires the vcproj files, they can just use VS2008 project files as template, and just change the version at the beginning of file.

--- branches/calllib-rewrite/synchronize-projects.bat
+++ branches/calllib-rewrite/synchronize-projects.bat
@@ -2,7 +2,7 @@
 
 set SYNCHRONIZER_JAR=contrib\VSCppProjectSynchronizer\VSCppProjectSynchronizer.jar
 
-echo Synchronizing VS2008 project files with VS2005 and VS2003...
+echo Synchronizing VS2008 project files with VS2005 ...
 
 rem Synchronize sipXcallLib and sipXtapi projects
 java -jar %SYNCHRONIZER_JAR% sipXcallLib\sipXcallLib-msvc9.vcproj sipXcallLib\sipXcallLib-msvc8.vcproj
@@ -10,46 +10,25 @@ java -jar %SYNCHRONIZER_JAR% sipXcallLib\sipXcallLibTest-msvc9.vcproj sipXcallLi
 java -jar %SYNCHRONIZER_JAR% sipXcallLib\sipXtapi-msvc9.vcproj sipXcallLib\sipXtapi-msvc8.vcproj
 java -jar %SYNCHRONIZER_JAR% sipXcallLib\sipXtapiTest-msvc9.vcproj sipXcallLib\sipXtapiTest-msvc8.vcproj
 
-java -jar %SYNCHRONIZER_JAR% sipXcallLib\sipXcallLib-msvc9.vcproj sipXcallLib\sipXcallLib-msvc71.vcproj
-java -jar %SYNCHRONIZER_JAR% sipXcallLib\sipXcallLibTest-msvc9.vcproj sipXcallLib\sipXcallLibTest-msvc71.vcproj
-java -jar %SYNCHRONIZER_JAR% sipXcallLib\sipXtapi-msvc9.vcproj sipXcallLib\sipXtapi-msvc71.vcproj
-java -jar %SYNCHRONIZER_JAR% sipXcallLib\sipXtapiTest-msvc9.vcproj sipXcallLib\sipXtapiTest-msvc71.vcproj
-
 rem Synchronize sipXmediaAdapterLib projects
 java -jar %SYNCHRONIZER_JAR% sipXmediaAdapterLib\sipXmediaAdapterLib-msvc9.vcproj sipXmediaAdapterLib\sipXmediaAdapterLib-msvc8.vcproj
 java -jar %SYNCHRONIZER_JAR% sipXmediaAdapterLib\sipXmediaAdapterLibTest-msvc9.vcproj sipXmediaAdapterLib\sipXmediaAdapterLibTest-msvc8.vcproj
 
-java -jar %SYNCHRONIZER_JAR% sipXmediaAdapterLib\sipXmediaAdapterLib-msvc9.vcproj sipXmediaAdapterLib\sipXmediaAdapterLib-msvc71.vcproj
-java -jar %SYNCHRONIZER_JAR% sipXmediaAdapterLib\sipXmediaAdapterLibTest-msvc9.vcproj sipXmediaAdapterLib\sipXmediaAdapterLibTest-msvc71.vcproj
-
 rem Synchronize sipXmediaLib projects
 java -jar %SYNCHRONIZER_JAR% sipXmediaLib\sipXmediaLib-msvc9.vcproj sipXmediaLib\sipXmediaLib-msvc8.vcproj
 java -jar %SYNCHRONIZER_JAR% sipXmediaLib\sipXmediaLibTest-msvc9.vcproj sipXmediaLib\sipXmediaLibTest-msvc8.vcproj
-
-java -jar %SYNCHRONIZER_JAR% sipXmediaLib\sipXmediaLib-msvc9.vcproj sipXmediaLib\sipXmediaLib-msvc71.vcproj
-java -jar %SYNCHRONIZER_JAR% sipXmediaLib\sipXmediaLibTest-msvc9.vcproj sipXmediaLib\sipXmediaLibTest-msvc71.vcproj
 
 rem Synchronize sipXportLib projects
 java -jar %SYNCHRONIZER_JAR% sipXportLib\sipXportLib-msvc9.vcproj sipXportLib\sipXportLib-msvc8.vcproj
 java -jar %SYNCHRONIZER_JAR% sipXportLib\sipXportLibTest-msvc9.vcproj sipXportLib\sipXportLibTest-msvc8.vcproj
 java -jar %SYNCHRONIZER_JAR% sipXportLib\sipXunitTestSuite-msvc9.vcproj sipXportLib\sipXunitTestSuite-msvc8.vcproj
 
-java -jar %SYNCHRONIZER_JAR% sipXportLib\sipXportLib-msvc9.vcproj sipXportLib\sipXportLib-msvc71.vcproj
-java -jar %SYNCHRONIZER_JAR% sipXportLib\sipXportLibTest-msvc9.vcproj sipXportLib\sipXportLibTest-msvc71.vcproj
-java -jar %SYNCHRONIZER_JAR% sipXportLib\sipXunitTestSuite-msvc9.vcproj sipXportLib\sipXunitTestSuite-msvc71.vcproj
-
 rem Synchronize sipXsdpLib projects
 java -jar %SYNCHRONIZER_JAR% sipXsdpLib\sipXsdpLib-msvc9.vcproj sipXsdpLib\sipXsdpLib-msvc8.vcproj
 java -jar %SYNCHRONIZER_JAR% sipXsdpLib\sipXsdpLibTest-msvc9.vcproj sipXsdpLib\sipXsdpLibTest-msvc8.vcproj
 
-java -jar %SYNCHRONIZER_JAR% sipXsdpLib\sipXsdpLib-msvc9.vcproj sipXsdpLib\sipXsdpLib-msvc71.vcproj
-java -jar %SYNCHRONIZER_JAR% sipXsdpLib\sipXsdpLibTest-msvc9.vcproj sipXsdpLib\sipXsdpLibTest-msvc71.vcproj
-
 rem Synchronize sipXtackLib projects
 java -jar %SYNCHRONIZER_JAR% sipXtackLib\sipXtackLib-msvc9.vcproj sipXtackLib\sipXtackLib-msvc8.vcproj
 java -jar %SYNCHRONIZER_JAR% sipXtackLib\sipXtackLibTest-msvc9.vcproj sipXtackLib\sipXtackLibTest-msvc8.vcproj
-
-java -jar %SYNCHRONIZER_JAR% sipXtackLib\sipXtackLib-msvc9.vcproj sipXtackLib\sipXtackLib-msvc71.vcproj
-java -jar %SYNCHRONIZER_JAR% sipXtackLib\sipXtackLibTest-msvc9.vcproj sipXtackLib\sipXtackLibTest-msvc71.vcproj
 
 echo Done.
